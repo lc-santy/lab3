6dbd6d8db5a94dd92ee119e702f3ea0f9fc54318
lc-santy <119831318+lc-santy@users.noreply.github.com>
2023-03-05 20:48:29 +0800

    Update NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model
 {
-    protected $table = 'news';
+    protected $table = 'lcsanty_news';
 	
     public function getNews($slug = false)
     {
