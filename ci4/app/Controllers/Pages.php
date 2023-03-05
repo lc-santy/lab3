233dc8c3441ee2e10aa148003f29c5e509c649d6
lc-santy <lcsanty@student.apc.edu.ph>
2023-03-05 19:27:58 +0800

    news

--- ci4/app/Controllers/Pages.php
+++ ci4/app/Controllers/Pages.php
@@ -5,9 +5,7 @@ namespace App\Controllers;
 class Pages extends BaseController
 {
     public function index()
-    {
-        return view('welcome_message');
-    }
+
 
         public function view($page = 'home')
 		{
