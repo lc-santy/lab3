095882ea4b322379cb9d521119b70325f1517016
lc-santy <lcsanty@student.apc.edu.ph>
2023-03-05 20:31:09 +0800

    update

--- ci4/app/Controllers/Pages.php
+++ ci4/app/Controllers/Pages.php
@@ -2,25 +2,23 @@
 
 namespace App\Controllers;
 
-
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Pages extends BaseController
 {
-    public function index()
+    // ...
 
+    public function view($page = 'home')
+    {
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
 
-        public function view($page = 'home')
-		{
-			if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
-				// Whoops, we don't have a page for that!
-				throw new PageNotFoundException($page);
-			}
+        $data['title'] = ucfirst($page); // Capitalize the first letter
 
-			$data['title'] = ucfirst($page); // Capitalize the first letter
-
-			return view('templates/header', $data)
-				. view('pages/' . $page)
-				. view('templates/footer');
-		}
-
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
+    }
 }
