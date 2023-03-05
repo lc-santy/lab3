ac5236e7387876ae2e9ce025f68658302a2ba4af
lc-santy <lcsanty@student.apc.edu.ph>
2023-03-05 13:33:43 +0800

    static pages

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -30,6 +30,10 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+use App\Controllers\Pages;
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
 
 /*
  * --------------------------------------------------------------------
---
+++ ci4/app/Views/pages/about.php
@@ -0,0 +1,1 @@
+boom tarat tarat
